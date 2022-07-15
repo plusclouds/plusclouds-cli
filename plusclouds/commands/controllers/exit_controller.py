413c1ece9e37ea85624e130d788b345d8f344211
Semih Yonet <semihyonet@gmail.com>
2022-07-15 13:55:27 +0200

    Exit Command completed #3

--- plusclouds/commands/controllers/exit_controller.py
+++ plusclouds/commands/controllers/exit_controller.py
@@ -1,11 +1,12 @@
+import sys
+
 from plusclouds.commands.controllers.abstract_controller import AbstractController
 from plusclouds.enums.parameter_types import ParameterType
 from plusclouds.util.token_util import set_token
 
 
-class SetTokenController(AbstractController):
-	parameter_types = [ParameterType.token]
+class ExitController(AbstractController):
+	parameter_types = []
 
 	def execute_command(self, *args, **kwargs):
-		token = self.get_parameter_values(**kwargs)[0]
-		set_token(token)
+		sys.exit(0)
