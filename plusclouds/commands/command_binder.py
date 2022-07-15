bd3393543e11c3c4de9daef2227dc2ee8b00c374
Semih Yonet <semihyonet@gmail.com>
2022-07-15 13:54:44 +0200

    Binded Controller to Command Binder with exit keyword

--- plusclouds/commands/command_binder.py
+++ plusclouds/commands/command_binder.py
@@ -3,10 +3,12 @@ from typing import Dict
 from plusclouds.commands.controllers.token_controller import SetTokenController
 from plusclouds.commands.controllers.list_controller import ListController
 from plusclouds.commands.controllers.create_controller import CreateController
+from plusclouds.commands.controllers.exit_controller import ExitController
 from plusclouds.commands.controllers.abstract_controller import AbstractController
 
 available_commands: Dict[str, AbstractController] = {
 	"set-token": SetTokenController(),
 	"list"     : ListController(),
-	"create"   : CreateController()
+	"create"   : CreateController(),
+	"exit"		: ExitController()
 }
