662097d79d77b943e982648adf7481f4efe36841
Semih Yonet <semihyonet@gmail.com>
2022-07-17 13:16:23 +0200

    binded cache into http client

--- plusclouds/gateway/http_client.py
+++ plusclouds/gateway/http_client.py
@@ -2,6 +2,19 @@ import requests
 
 from plusclouds.settings import plusclouds_url
 from plusclouds.util.token_util import get_token
+from plusclouds.gateway.http_cache import cache_constructor
+
+get_dict = {}
+get_cache = cache_constructor(get_dict)
+
+post_dict = {}
+post_cache = cache_constructor(post_dict)
+
+delete_dict = {}
+delete_cache = cache_constructor(delete_dict)
+
+options_dict = {}
+options_cache = cache_constructor(options_dict)
 
 
 class HttpGateway:
@@ -11,11 +24,13 @@ class HttpGateway:
 		self.latest_request = None
 		self.option_request = None
 
+	@get_cache
 	def get(self, url: str = "") -> requests.Response:
 		full_url = plusclouds_url + url
 		self.latest_request = requests.get(full_url, headers={"Authorization": "Bearer " + self.token.strip()})
 		return self.latest_request
 
+	@post_cache
 	def post(self, url: str = "", body=None) -> requests.Response:
 		if body is None:
 			body = {}
@@ -25,6 +40,7 @@ class HttpGateway:
 											headers={"Authorization": "Bearer " + self.token.strip()})
 		return self.latest_request
 
+	@options_cache
 	def options(self, url: str = "") -> requests.Response:
 		self.option_request = requests.request("OPTIONS", self.base_url + url)
 		return self.option_request
