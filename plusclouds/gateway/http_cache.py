c7218b6e4d15c4abf3bbb4c16915b2e156b4b15a
Semih Yonet <semihyonet@gmail.com>
2022-07-29 21:44:31 +0200

    Improved cache mechanism

--- plusclouds/gateway/http_cache.py
+++ plusclouds/gateway/http_cache.py
@@ -1,11 +1,22 @@
 from plusclouds.util.token_util import token_id
 
 
+def get_key(obj: dict, key: str) -> str:
+	try:
+		return obj[key]
+	except KeyError as e:
+		return ""
+
+
 def cache_constructor(cache_dict: dict):
 	def cache_decorator(func):
 
 		def callback(*args, **kwargs):
-			url = kwargs.get("url", "")
+
+			url = get_key(kwargs,"url")
+
+			url += get_key(kwargs,"query")
+			url += get_key(kwargs,"body")
 
 			if url == "":
 				url = "_"
