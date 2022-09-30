6ccdefecec517e5e2ec3de4976e8160dfdcc55d5
zbudin <dev.budin.zekican@gmail.com>
2022-09-30 20:06:38 +0100

    Removed create controller and create functionality

--- plusclouds/commands/command_binder.py
+++ plusclouds/commands/command_binder.py
@@ -2,7 +2,6 @@ from typing import Dict
 
 from plusclouds.commands.controllers.token_controller import SetTokenController
 from plusclouds.commands.controllers.list_controller import ListController
-from plusclouds.commands.controllers.create_controller import CreateController
 from plusclouds.commands.controllers.exit_controller import ExitController
 from plusclouds.commands.controllers.ssh_controller import SSHController
 from plusclouds.commands.controllers.upload_token_controller import UploadTokenController
@@ -12,7 +11,6 @@ available_commands: Dict[str, AbstractController] = {
 	"set-token" : SetTokenController(),
 	"upload-ssh": UploadTokenController(),
 	"list"      : ListController(),
-	"create"    : CreateController(),
 	"exit"      : ExitController(),
 	"ssh"       : SSHController()
 }
